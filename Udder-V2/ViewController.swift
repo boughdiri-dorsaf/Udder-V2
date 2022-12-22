00cc72bbb973406138cc10ea898cd9d0e607d4a6
boughdiri-dorsaf <boughdiridorsaf12@gmail.com>
2022-12-22 14:36:43 +0100

    Add ViewDidDisappear

--- Udder-V2/ViewController.swift
+++ Udder-V2/ViewController.swift
@@ -21,5 +21,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
 }
 
